4451ef228bacae2c48758b34ee9aacca7725c4a5
Oscar Fernández Muñoz <oscarfernandezm95@gmail.com>
2022-09-27 14:19:47 -0500

    Add dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,15 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   // entry: './src/index.js', we have the default file so we can omit this
+  devServer: {
+    static: './build',
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
   ],
+  mode: 'development',
   output: {
     path: path.resolve(__dirname, 'build') // Changes the directory name
   },
@@ -18,5 +22,8 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
     ],
+  },
+  optimization: {
+    runtimeChunk: 'single',
   },
 }
